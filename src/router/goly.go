9b18a0005120473632aade41f3d7f240ea1932b3
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-06-25 22:22:38 +0100

    adding goly router

---
+++ src/router/goly.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"Go_shortener/src/controller"
+	"Go_shortener/src/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutesGoly(r *gin.RouterGroup) {
+	// r.POST("/", controller.NewGoly)
+	// r.GET("/:id", controller.GetGoly)
+	// r.PUT("/:id", controller.UpdateGoly)
+	// r.DELETE("/:id", controller.DeleteGoly)
+	r.POST("/", middleware.OptionalAuth, controller.NewGoly)
+	r.GET("/", middleware.OptionalAuth, controller.GetAllGolies)
+	r.GET("/:id", middleware.OptionalAuth, controller.GetGoly)
+	r.PUT("/:id", middleware.RequireAuth, controller.UpdateGoly)
+	r.DELETE("/:id", middleware.RequireAuth, controller.DeleteGoly)
+}
