c2f7ca36870139645b9e55f22a32b3dce0ea735f
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-06-29 21:30:13 +0100

    adding a home page

--- main.go
+++ main.go
@@ -5,13 +5,11 @@ import (
 	"Go_shortener/src/router"
 	"Go_shortener/src/setup"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
-
-func init() {
-}
 
 func main() {
 	err := godotenv.Load()
@@ -24,9 +22,21 @@ func main() {
 	db := setup.GetDB()
 	db.AutoMigrate(&models.Goly{})
 	db.AutoMigrate(&models.User{})
+
+	// router for homepage
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"title":   "Welcome to Goly Shortener API",
+			"Status":  "currently running",
+			"message": "To view all public golies head to the /golies"})
+	})
+
+	// other router families
 	users := r.Group("/users")
 	golies := r.Group("/golies")
 	redirect := r.Group("/r")
+
+	// register the routes
 	router.RegisterRoutesUser(users)
 	router.RegisterRoutesGoly(golies)
 	router.RegisterRoutesRedirect(redirect)
