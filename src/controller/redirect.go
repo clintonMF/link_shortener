ab40dacfe13d047e32c210ef28172b22c045dad2
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-10-12 16:33:22 +0100

    feat:created the updateClick function

--- src/controller/redirect.go
+++ src/controller/redirect.go
@@ -3,21 +3,51 @@ package controller
 import (
 	"Go_shortener/src/models"
 	"Go_shortener/src/utils"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+func updateClicks(url string) {
+	// this function updates the click in the background
+	goly, err := models.GetGolyByURL(url)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	goly.Clicked += 1
+
+	err = models.UpdateGoly(goly)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	cache.Set(url, goly.Redirect, 0)
+	fmt.Println("clicks updated")
+}
+
 func Redirect(c *gin.Context) {
 	golyUrl := c.Param("redirect")
-	goly, err := models.GetGolyByURL(name + golyUrl)
+	value, found := cache.Get(name + golyUrl)
 
+	if found {
+		/*
+			if the cache has the redirect link
+			redirect the user and perform update the click value
+			in the background
+		*/
+		go updateClicks(name + golyUrl)
+		c.Redirect(http.StatusTemporaryRedirect, value.(string))
+		return
+	}
+
+	goly, err := models.GetGolyByURL(name + golyUrl)
 	if err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
 	goly.Clicked += 1
+	cache.Set(name+golyUrl, goly.Redirect, 0)
 
 	err = models.UpdateGoly(goly)
 
